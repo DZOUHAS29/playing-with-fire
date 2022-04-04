ae0754e9ec49cedb6c2621070758cd2a9a855437
Tomas Kubin <kubin.tom@email.cz>
2022-04-04 12:53:11 +0200

    Play withe fire

---
+++ PlayWithFire/GamePanel.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PlayWithFire
+{
+    partial class GamePanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCanvas
+            // 
+            this.pbCanvas.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbCanvas.Location = new System.Drawing.Point(109, 76);
+            this.pbCanvas.Name = "pbCanvas";
+            this.pbCanvas.Size = new System.Drawing.Size(100, 50);
+            this.pbCanvas.TabIndex = 0;
+            this.pbCanvas.TabStop = false;
+            // 
+            // GamePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbCanvas);
+            this.Name = "GamePanel";
+            this.Text = "GamePanel";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCanvas;
+    }
+}
+
---
+++ PlayWithFire/GamePanel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayWithFire
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+    }
+    public partial class GamePanel : Form
+    {
+        // columns - id, name, surname, email
+        public GamePanel()
+        {
+            InitializeComponent();
+
+            this.Size = new Size(1000, 1000);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            this.pbCanvas.Location = new Point(0, 0);
+            this.pbCanvas.Size = this.ClientSize;
+
+
+        }
+    }
+}
